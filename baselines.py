e7e8cc91329f20f54cfc702892f04df4d9e1081b
Sergei Volodin <sergei.volodin@epfl.ch>
2019-06-02 19:50:40 +0200

    Add CPO stub

--- baselines.py
+++ baselines.py
@@ -32,3 +32,35 @@ class ConstrainedRandomAgent(ConstrainedAgent):
         return {'foo': 'bar'}
         pass
 
+class ConstrainedPolicyOptimization(ConstrainedAgent):
+    """ CPO agent """
+    def __init__(self, env):
+        """ Initialize for environment """
+        super(ConstrainedRandomAgent, self).__init__(env)
+
+    def sample_action(self, observation):
+        """ Sample an action given observation, typically runs on a GPU """
+        assert observation is not None
+        return np.random.choice(self.n_actions)
+
+    def episode_start(self):
+        """ Called each time a new episode starts """
+        pass
+
+    def episode_end(self):
+        """ Called each time an episode ends """
+        pass
+
+    def process_feedback(self, state, action, reward, cost, state_new, done, info):
+        """ Called inside the train loop, typically just stores the data """
+        pass
+
+    def train_start(self):
+        """ Called before one training phase """
+        pass
+
+    def train(self):
+        """ Train method, typically runs on a GPU """
+        return {'foo': 'bar'}
+        pass
+
